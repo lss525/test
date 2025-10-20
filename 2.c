4a8bebff981825f74dac6c1764451ac2c3e31e62
lss525 <1282852660@qq.com>
2025-10-20 20:42:07 +0800

    2.12

--- 2.c
+++ 2.c
@@ -1,4 +1,84 @@
-#include<stdio.h>
-int main(){
-    
-}
+//一：
+// #include<stdio.h>
+// int main(){
+//     printf("hy l\n");
+//     printf("hy\nl\n");
+//     printf("hy ");
+//     printf("l");
+//     return 0;
+// }
+//二：
+// #include<stdio.h>
+// int main(){
+//     char s[4]="lhy";
+//     char* p=s;
+//     printf("%s\n%p",s,p);
+//     return 0;
+// }
+//三：
+// #include<stdio.h>
+// int main(){
+//     int n=18;
+//     printf("%d\n%d",n,365*n);
+//     return 0;
+// }
+//四：
+// #include<stdio.h>
+// void jolly(char s[]){
+//     printf("%s\n",s);
+//     printf("%s\n",s);
+//     printf("%s\n",s);
+// }
+// void deny(void){
+//     printf("Which nobody can deny");
+// }
+// int main(){
+//     char s[50]="For he's a joiiy good fellow!";
+//     jolly(s);
+//     deny();
+//     return 0;
+// }
+//五：
+// #include<stdio.h>
+// void br(void){
+//     printf("Brazil,Russia");
+// }
+// void ic(voud){
+//     printf("India,China\n");
+// }
+// int main(){
+//     br();
+//     printf(",");
+//     ic();
+//     ic();
+//     br();
+//     return 0;
+// }
+//六：
+// #include<stdio.h>
+// int main(){
+//     int toes=10;
+//     printf("%d %d %d",toes,2*toes,toes*toes);
+//     return 0;
+// }
+//七
+// #include<stdio.h>
+// int main(){
+//     printf("Smill!Smill!Smill!\nSmill!Smill!\nSmill!");
+//     return 0;
+// }
+//八
+// #include<stdio.h>
+// void two(void){
+//     printf("two\nthree\n");
+// }
+// void one_three(void){
+//     printf("one\n");
+//     two();
+// }
+// int main(){
+//     printf("starting now\n");
+//     one_three();
+//     printf("done");
+//     return 0;
+// }
