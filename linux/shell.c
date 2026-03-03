fc73fa273541d2994a548e4d0f66caaaf66e9d2c
lss525 <1282852660@qq.com>
2026-03-03 11:30:34 +0800

    shell

--- linux/shell.c
+++ linux/shell.c
@@ -9,11 +9,10 @@
 #define MAX_ARGS 64
 
 
-void execute_command(char *input) {
+void mycommand(char *input) {
     pid_t pid;
     char *args[MAX_ARGS];
     int i = 0;
-    
     // 分割命令参数
     char *token = strtok(input, " ");
     while (token != NULL && i < MAX_ARGS - 1) {
@@ -27,18 +26,50 @@ void execute_command(char *input) {
     if (pid == 0) {
         // 子进程
         execvp(args[0], args);
-        // 如果execvp失败
         perror("execvp failed");
         exit(1);
     } else if (pid > 0) {
-        // 父进程等待子进程结束
         wait(NULL);
     } else {
         perror("fork failed");
     }
 } 
 
+// 检查是否为内置命令
+int is_builtin(char *command) {
+    char *builtins[] = {"cd", "help", "exit", "pwd", "echo"};
+    for (int i = 0; i < 5; i++) {
+        if (strcmp(command, builtins[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
+// 执行内置命令
+void execute_builtin(char **args) {
+    if (strcmp(args[0], "cd") == 0) {
+        if (args[1] == NULL) {
+            chdir(getenv("HOME"));
+        } else {
+            if (chdir(args[1]) != 0) {
+                perror("cd failed");
+            }
+        }
+    } else if (strcmp(args[0], "help") == 0) {
+        printf("简单的 Shell 实现\n");
+        printf("支持的命令: cd, pwd, echo, exit, help\n");
+    } else if (strcmp(args[0], "pwd") == 0) {
+        char cwd[1024];
+        getcwd(cwd, sizeof(cwd));
+        printf("%s\n", cwd);
+    } else if (strcmp(args[0], "echo") == 0) {
+        for (int i = 1; args[i] != NULL; i++) {
+            printf("%s ", args[i]);
+        }
+        printf("\n");
+    }
+}
 
 void print_prompt() {
     printf("myshell> ");
