7ba2b1633808cdf0b242f01a12ecee828bfffec3
lss525 <1282852660@qq.com>
2026-05-05 20:12:57 +0800

    net

---
+++ linux/FTP.c
@@ -0,0 +1,22 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <time.h>
+#include <ctype.h>
+
+int main(int argc,char*argv[]){
+    
+    //根目录
+    //socket
+    //
+
+}
