c7d373c69e78270b41f5531c23db1a0223060981
lss525 <1282852660@qq.com>
2026-05-06 20:10:54 +0800

    net

--- linux/FTP.c
+++ linux/FTP.c
@@ -13,9 +13,21 @@
 #include <time.h>
 #include <ctype.h>
 
+#define DK 2100
+#define HCHANG 1024
+#define GENMULU "/tmp/ftp_root"
+
+int fuwu_sock=-1;
+int yunxing=1;
+
 int main(int argc,char*argv[]){
-    
+    signal(SIGINT,xinhao_chuli);
+    signal(SIGTERM,xinhao_chuli);
+    signal(SIGPIPE,SIG_IGN);
+    int dk=DK;
+    if(argc>1)dk=atoi(argv[1]);
     //根目录
+    mkdir(GENMULU,0755);
     //socket
     //
 
