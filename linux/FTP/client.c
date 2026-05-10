8e3c99ec13a69a983161be5f9348c29d378a926f
lss525 <1282852660@qq.com>
2026-05-10 20:56:56 +0800

    FTP

---
+++ linux/FTP/client.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define HCHANG 4096
+
+int kongzhi_sock=-1;
+int main(int argc,char*argv[]){
+    char* ip="127.0.0.1";
+    int dk=2100;
+    
+}
